2b54a9a4b29a3df29f6441556a0fbf6a99d715eb
drancon <39079751+drancon@users.noreply.github.com>
2022-06-25 19:40:59 +0900

    minor fix

--- run_demo.sh
+++ run_demo.sh
@@ -1,5 +1,5 @@
 python demo.py \
 ./configs/segformer/segformer_mit-b5_8x1_1024x1024_160k_cityscapes.py \
-./weights/segformer_mit-b5_8x1_1024x1024_160k_cityscapes.pth \
+./ckpts/segformer_mit-b5_8x1_1024x1024_160k_cityscapes.pth \
 --data_dir ./demo \
 --save_root ./demo/results
